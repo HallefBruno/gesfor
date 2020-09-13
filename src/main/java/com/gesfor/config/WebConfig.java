869f2479514f4b895290884c21c92742272beb10
brunohallef <brunohallef@gmail.com>
2020-09-13 15:49:36 -0300

    Adicionando um controller Index

--- src/main/java/com/gesfor/config/WebConfig.java
+++ src/main/java/com/gesfor/config/WebConfig.java
@@ -50,6 +50,6 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("Home");
+        registry.addViewController("/").setViewName("Index");
     }
 }
---
+++ src/main/java/com/gesfor/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.gesfor.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author hallef
+ */
+@Controller
+public class IndexController {
+    
+    @RequestMapping("/")
+    public ModelAndView index() {
+        return new ModelAndView("Index");
+    }
+}
