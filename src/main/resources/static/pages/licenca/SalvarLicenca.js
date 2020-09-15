a68cbef038874bbc62e2a5242c54170f5393631d
hallef.bruno <hallef.bruno@CRP-OPERACAO-15.crp.com>
2020-09-15 14:27:55 -0300

    Criando função para realizar validação via click btn

--- src/main/resources/static/pages/licenca/SalvarLicenca.js
+++ src/main/resources/static/pages/licenca/SalvarLicenca.js
@@ -1,99 +1,57 @@
-/* global messageAtribbuteError */
-
 $(function () {
-
+    
     validarComposDeEntrada();
+    
+    $("#btnSalvarLicenca").click(function () {
 
-        $("#btnSalvarLicenca").click(function () {
-        
-            $("#msgError").html("");
+        $("#msgError").html("");
 
-            var licenca = {cnpj: $("#cnpj").val()};
+        var licenca = {'cnpj': $("#cnpj").val(), 'dataCadastro': new Date(), 'email': $("#email").val(), 'status': true, 'telefone': $("#telefone").val()};
 
-            $.ajax({
+        $.ajax({
 
-                method: "POST",
-                url: "licencas/salvar",
-                data: JSON.stringify(licenca),
-                contentType: "application/json",
-                dataType: "json",
+            method: "POST",
+            url: "licencas/salvar",
+            data: JSON.stringify(licenca),
+            contentType: "application/json",
+            dataType: "json",
 
-                success: function (data, textStatus, jqXHR) {
-                    alert("Registro salvo com sucesso");
-                },
+            success: function (data, textStatus, jqXHR) {
+                //alert("Registro salvo com sucesso");
+                swal("Here's a message!");
+            },
 
-                error: function (xhr, status, error) {
+            error: function (xhr, status, error) {
+                //console.log(validarComposDeEntrada);
+            }
 
-                    
-//                    var response = xhr.responseText;
-//                    var objError = JSON.parse(response);
-//                    var messageErro = [];
-//                    
-//                    Object.keys(objError).forEach(function (key) {
-//                        messageErro.push({'attribute':key,'message':objError[key]});
-//                    });
-//                    
-//getMessageErroAttribute();
-                    //console.log(messageAtribbuteError);
-//                    
-//                    $("#msgError").append(messageErro);
-                }
-
-            });
-        
         });
-});
-//
-////$(function () {
-//    
-//    
-//    
-//    $("#btnSalvarLicenca").click(function () {
-//        
-//        $("#msgError").html("");
-//        
-//        var licenca = {cnpj: $("#cnpj").val()};
-//        
-//        $.ajax({
-//            
-//            method: "POST",
-//            url: "licencas/salvar",
-//            data: JSON.stringify(licenca),
-//            contentType: "application/json",
-//            dataType: "json",
-//            
-//            success: function (data, textStatus, jqXHR) {
-//                alert("Registro salvo com sucesso");
-//            },
-//            
-//            error: function (xhr, status, error) {
-//                console.log(xhr.responseText);
-//                $("#msgError").html(xhr.responseText);
-//            }
-//            
-//        });
-//        
-//    });
-//    
-//})
 
-//(function () {
-//   'use strict';
-//    var inputs = document.getElementsByClassName('validar');
-//    Array.prototype.filter.call(inputs, function (input) {
-//        
-//        input.addEventListener('blur', function (event) {
-//            
-//            input.classList.remove('is-invalid');
-//            input.classList.remove('is-valid');
-//            
-//            if (input.checkValidity() === false) {
-//                input.classList.add('is-invalid');
-//            } else {
-//                input.classList.add('is-valid');
-//            }
-//            
-//        }, false);
-//    });
-//    
-//})();
+    });
+
+
+});
+
+function latLong() {
+
+    var options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+}
+
+function success(pos) {
+    var crd = pos.coords;
+    console.log(crd);
+    console.log('Sua posição atual é:');
+    console.log('Latitude : ' + crd.latitude);
+    console.log('Longitude: ' + crd.longitude);
+    console.log('Mais ou menos ' + crd.accuracy + ' metros.');
+}
+
+function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+}
